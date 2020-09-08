ca3aae8e49c377b57268768b340a09fb20e34357
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-08 10:13:46 +0100

    add path /data with method post

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/home', (req, res) => {
     res.send("Welcome to my first back end !")
 })
 
+app.post("/data", (req, res) => {
+    console.log("from post /data")
+})
+
 app.listen(3000, () => {
     console.log("app listening on port:3000")
 })
