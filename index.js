65590cd1f092bc08ebbc96f00a86661f9dce1d0b
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-08 13:25:01 +0100

    add put,patch,delete with path /profile

--- index.js
+++ index.js
@@ -20,6 +20,70 @@ app.post("/profile", (req, res) => {
     res.send(data)
 })
 
+app.get("/profile", (req, res) => {
+    let data = {
+        name: 'Jane',
+        batch: '17.2',
+        email: 'janedoe@mail.com'
+    }
+
+    res.send(data)
+})
+
+app.patch("/profile", (req, res) => {
+    let data = {
+        name: 'Jane',
+        batch: '17.2',
+        email: 'janedoe@mail.com'
+    }
+
+    const result = update({ data })
+
+    if (result) {
+        res.send(result)
+    } else {
+        res.send({
+            message: "data gagal di update",
+        })
+    }
+})
+
+app.put("/profile", (req, res) => {
+    const { name, batch, email } = data
+
+    let namee = "iqbal"
+    let batchh = "20"
+    let emaill = "iqbalathorid@gmail.com"
+    const result = update({
+        name: namee,
+        batch: batchh,
+        email: emaill
+    })
+
+    if (result.length != data.length) {
+        res.send({
+            message: "error",
+            err: err.message
+        })
+    } else {
+        res.send(result)
+    }
+
+})
+
+app.delete("/profile", (req, res) => {
+    const result = delete (data)
+
+    if (result) {
+        res.send({ message: "null" })
+    } else {
+        res.send({
+            message: "gagal delete",
+            err: err.message
+        })
+    }
+})
+
 
 app.listen(3000, () => {
     console.log("app listening on port:3000")
