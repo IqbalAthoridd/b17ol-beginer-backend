4692e7f0d044a9be39a8e8bc5aac988ed66af1ae
Iqbal Athorid <iqbalathoridd@gmail.com>
2020-09-08 10:52:37 +0100

    fix post path /data

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const app = express()
 
+app.use(express.urlencoded({
+    extended: true,
+}))
+app.use(express.json())
+
 app.get('/home', (req, res) => {
     res.send("Welcome to my first back end !")
 })
 
-app.post("/data", (req, res) => {
-    console.log("from post /data")
+app.post("/data", async (req, res) => {
+    const data = await req.body
+    console.log(data)
 })
+
 
 app.listen(3000, () => {
     console.log("app listening on port:3000")
